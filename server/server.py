66635c84e5222d1174abe4227c9061f0cf9ad0e7
Kevin Martin <nucleus@tritium.168.1.9>
2020-04-09 06:43:31 -0500

    Remove URL from Hug documentation

--- server/server.py
+++ server/server.py
@@ -3,18 +3,18 @@ import os
 import falcon
 from core.plotter import CommandPlotter, CommandError, CountryError, DaysError, PlottingError
 
-URL = os.environ['CORONAPLOTTER_URL']
-
-@hug.get(URL+'plot', examples=['command=total&country=chile&days=2','command=recovered&country=united+kingdom&days=0'])
+@hug.get(examples=['command=total&country=chile&days=2','command=recovered&country=united+kingdom&days=0'])
 def plot(command, country, days=0):
     """ Returns the plot image URL in the format {'status': 'ok', 'message': 'url.to/plot/image'}
     Accepted values for <command> are: (total|new|deaths|recovered)
     <days> must be in the (0,8) range. This includes 0 and 7."""
+
     return {'status': 'ok', 'message': CommandPlotter().execute(command, country, days)}
 
-@hug.get(URL+'info')
+@hug.get()
 def info():
     """Returns the plot image URL in the format {'status': 'ok', 'message': 'large text'}"""
+
     text =  """
             Plotter created by:
             Kevin Martin
