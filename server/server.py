1550d1ba7a6d9c616ef767a70c2a8238e947a0ca
Kevin Martin <nucleus@tritium.168.1.9>
2020-04-09 06:14:17 -0500

    Remove unused documentation directive

--- server/server.py
+++ server/server.py
@@ -1,13 +1,18 @@
 import hug
+import os
 import falcon
 from core.plotter import CommandPlotter, CommandError, CountryError, DaysError, PlottingError
 
-@hug.get(examples=['command=total&country=chile&days=2','command=recovered&country=united+kingdom&days=0'], response={'status': 'ok'})
-def plot(command: hug.types.one_of(['total', 'new', 'deaths', 'recovered']), country, days: hug.types.in_range(0, 8) = 0):
-    """Returns the plot image URL in the format {'status': 'ok', 'message': 'url.to/plot/image'}"""
+URL = os.environ['CORONAPLOTTER_URL']
+
+@hug.get(URL+'plot', examples=['command=total&country=chile&days=2','command=recovered&country=united+kingdom&days=0'])
+def plot(command, country, days=0):
+    """ Returns the plot image URL in the format {'status': 'ok', 'message': 'url.to/plot/image'}
+    Accepted values for <command> are: (total|new|deaths|recovered)
+    <days> must be in the (0,8) range. This includes 0 and 7."""
     return {'status': 'ok', 'message': CommandPlotter().execute(command, country, days)}
 
-@hug.get()
+@hug.get(URL+'info')
 def info():
     """Returns the plot image URL in the format {'status': 'ok', 'message': 'large text'}"""
     text =  """
