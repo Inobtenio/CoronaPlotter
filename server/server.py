b0f7620c6f0a3f7a592fb048a9bf32557d0397b3
Kevin Martin <nucleus@tritium.168.1.9>
2020-04-09 05:20:48 -0500

    Improve API documentation

--- server/server.py
+++ server/server.py
@@ -2,12 +2,14 @@ import hug
 import falcon
 from core.plotter import CommandPlotter, CommandError, CountryError, DaysError, PlottingError
 
-@hug.get()
-def plot(command, country, days=0):
+@hug.get(examples=['command=total&country=chile&days=2','command=recovered&country=united+kingdom&days=0'], response={'status': 'ok'})
+def plot(command: hug.types.one_of(['total', 'new', 'deaths', 'recovered']), country, days: hug.types.in_range(0, 8) = 0):
+    """Returns the plot image URL in the format {'status': 'ok', 'message': 'url.to/plot/image'}"""
     return {'status': 'ok', 'message': CommandPlotter().execute(command, country, days)}
 
 @hug.get()
 def info():
+    """Returns the plot image URL in the format {'status': 'ok', 'message': 'large text'}"""
     text =  """
             Plotter created by:
             Kevin Martin
