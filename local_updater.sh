63c73335e979d695a862a72bdb13b6a5b7baf969
Kevin Martin <knmartinm@gmail.com>
2020-04-11 18:54:58 -0500

    Remove hardcoded path from local updater

--- local_updater.sh
+++ local_updater.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo $1 >> 'core/data/peru.csv'
-/home/nucleus/projects/covid-19/plotter/plotter/bin/python core/local_updater.py
+$(pwd)/plotter/bin/python core/local_updater.py
